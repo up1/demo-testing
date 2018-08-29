4887cf875848abe475afd1d977a9a734133f370f
somkiat <somkiat.p@gmail.com>
2018-08-29 23:58:07 +0700

    Example with chart

---
+++ examples/image.md
@@ -0,0 +1,4 @@
+# ทดสอบจาก web
+
+~~~yaml example="image" fixture="../public/call_web.js"
+~~~
---
+++ public/call_web.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = function (params) {
+	return `https://up1.github.io/demo-testing/image.html`;
+};
